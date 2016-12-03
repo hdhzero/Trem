0e147a6c9c6052f28a0b6c3ed3a8dfff178fbcf9
Hadley Siqueira <hdhzero@gmail.com>
2016-12-02 23:13:45 -0300

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,37 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    trem1 = new Trem(1,144,130);
+    trem2 = new Trem(2,144,230);
+    connect(trem1,SIGNAL(updateGUI(int,int,int)),SLOT(updateInterface(int,int,int)));
+    trem1->start();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::drawRails() {
+
+}
+
+void MainWindow::updateInterface(int id, int x, int y)
+{
+    switch(id){
+        case 1:
+            ui->labelTrem01->setGeometry(x,y,15,15);
+            break;
+        case 2:
+            break;
+        default:
+            break;
+    }
+}
+
---
+++ trem.cpp
@@ -0,0 +1,67 @@
+#include "trem.h"
+
+Trem::Trem(int id, int x, int y)
+{
+    this->id = id;
+    this->x = x;
+    this->y = y;
+    velocidade = 70;
+    enable = true;
+}
+
+Trem::~Trem()
+{
+    threadTrem.join();
+}
+
+void Trem::setVelocidade(int velocidade)
+{
+    this->velocidade = velocidade;
+}
+
+void Trem::setEnable(bool enable)
+{
+    this->enable = enable;
+}
+
+void Trem::start()
+{
+    threadTrem = std::thread(&Trem::run,this);
+}
+
+void Trem::run()
+{
+    while(true){
+        switch(id){
+        case 1:
+            if (enable)
+            {
+                emit updateGUI(id,x,y);
+
+                if (y == 95 && x > 144)
+                    x -= 1;
+                else if (x == 144 && y < 193)
+                    y += 1;
+                else if (y == 193 && x < 242)
+                    x += 1;
+                else
+                    y -= 1;
+            }
+            break;
+        case 2:
+            if (enable) {
+                emit updateGUI(id,x,y);
+
+                if (x == 144 && y > 193) {
+                    y -= 1;
+                } else if (y == 193 && x ) {}
+            }
+
+            break;
+        default:
+            break;
+        }
+        this_thread::sleep_for(chrono::milliseconds(velocidade));
+    }
+}
+
